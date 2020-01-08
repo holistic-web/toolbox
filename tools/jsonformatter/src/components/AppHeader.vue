5564fb3951f5da14c776908fc8437dd25a6359e1
Andrew Lewis <andrew12lewis@gmail.com>
2020-01-08 17:00:47 +0000

    feat: minimal navbar as header

--- tools/jsonformatter/src/components/AppHeader.vue
+++ tools/jsonformatter/src/components/AppHeader.vue
@@ -1,41 +1,20 @@
 <template>
   <div class="AppHeader">
     <div>
-        <b-navbar toggleable="lg" type="dark" variant="info">
-            <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-            <b-collapse id="nav-collapse" is-nav>
+        <b-navbar type="dark" variant="dark">
+            <!-- left partition -->
             <b-navbar-nav>
-                <b-nav-item href="#">Link</b-nav-item>
-                <b-nav-item href="#" disabled>Disabled</b-nav-item>
+                <b-nav-item href="#">JSONFormatter</b-nav-item>
+                <!-- Navbar dropdowns -->
+                <b-nav-item-dropdown text="" right>
+                    <b-dropdown-item href="#">Other Service</b-dropdown-item>
+                    <b-dropdown-item href="#">Other Service</b-dropdown-item>
+                </b-nav-item-dropdown>
             </b-navbar-nav>
-
-            <!-- Right aligned nav items -->
+            <!-- right partition-->
             <b-navbar-nav class="ml-auto">
-                <b-nav-form>
-                <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-                </b-nav-form>
-
-                <b-nav-item-dropdown text="Lang" right>
-                <b-dropdown-item href="#">EN</b-dropdown-item>
-                <b-dropdown-item href="#">ES</b-dropdown-item>
-                <b-dropdown-item href="#">RU</b-dropdown-item>
-                <b-dropdown-item href="#">FA</b-dropdown-item>
-                </b-nav-item-dropdown>
-
-                <b-nav-item-dropdown right>
-                <!-- Using 'button-content' slot -->
-                <template v-slot:button-content>
-                    <em>User</em>
-                </template>
-                <b-dropdown-item href="#">Profile</b-dropdown-item>
-                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-                </b-nav-item-dropdown>
+                <img alt="Vue logo" src="../assets/logo.png">
             </b-navbar-nav>
-            </b-collapse>
         </b-navbar>
     </div>
   </div>
@@ -47,7 +26,3 @@ export default {
   name: 'AppHeader',
 }
 </script>
-
-<style scoped>
-
-</style>
