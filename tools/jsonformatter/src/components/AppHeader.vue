13191219f6e6b0a78cfcd2bcfd6481b1a6732d29
Andrew Lewis <andrew12lewis@gmail.com>
2020-01-08 18:14:42 +0000

    chore: cleanup

--- tools/jsonformatter/src/components/AppHeader.vue
+++ tools/jsonformatter/src/components/AppHeader.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="app-header">
-    <div>
-        <b-navbar type="dark" variant="dark">
-            <!-- left partition -->
-            <b-navbar-nav>
-                <b-nav-item href="#">JSONFormatter</b-nav-item>
-                <!-- Navbar dropdowns -->
-                <b-nav-item-dropdown text="" right>
-                    <b-dropdown-item href="#">Other Service</b-dropdown-item>
-                    <b-dropdown-item href="#">Other Service</b-dropdown-item>
-                </b-nav-item-dropdown>
-            </b-navbar-nav>
-            <!-- right partition-->
-            <b-navbar-nav class="ml-auto">
-                <img alt="Vue logo" src="../assets/logo.png">
-            </b-navbar-nav>
-        </b-navbar>
-    </div>
+    <b-navbar type="dark" variant="dark">
+        <!-- left partition -->
+        <b-navbar-nav>
+            <b-nav-item href="#">JSONFormatter</b-nav-item>
+            <!-- Navbar dropdowns -->
+            <b-nav-item-dropdown text="" right>
+                <b-dropdown-item href="#">Other Service</b-dropdown-item>
+                <b-dropdown-item href="#">Other Service</b-dropdown-item>
+            </b-nav-item-dropdown>
+        </b-navbar-nav>
+        <!-- right partition-->
+        <b-navbar-nav class="ml-auto">
+            <img alt="Vue logo" src="../assets/logo.png">
+        </b-navbar-nav>
+    </b-navbar>
   </div>
 </template>
 
