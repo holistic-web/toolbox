e292cd2f76c8194aa614c241c3d377a7ab29258b
Andrew Lewis <andrew12lewis@gmail.com>
2020-01-08 18:33:20 +0000

    chore: whitespace

--- tools/jsonformatter/src/components/Page.vue
+++ tools/jsonformatter/src/components/Page.vue
@@ -1,15 +1,15 @@
 <template>
 	<div class="page" >
 		<b-jumbotron id="jumbotron">
-            <b-form-group
+			<b-form-group
 				label-cols-sm="1"
 				label="Add your JSON here:"
 				label-align-sm="center"
 				label-for="input-json">
-                <b-form-textarea class="page__input" id="input-json" rows="12" max-rows="100"></b-form-textarea>
-                <b-button pill variant="outline-dark" @click="submit">Submit</b-button>
-            </b-form-group>
-            <img class="page__arrow" alt="arrow" src="../assets/arrow.svg">
+				<b-form-textarea class="page__input" id="input-json" rows="12" max-rows="100"></b-form-textarea>
+				<b-button pill variant="outline-dark" @click="submit">Submit</b-button>
+			</b-form-group>
+			<img class="page__arrow" alt="arrow" src="../assets/arrow.svg">
 		</b-jumbotron>
 	</div>
 </template>
@@ -28,18 +28,18 @@ export default {
 <style lang="scss">
 
 #jumbotron{
-    margin-bottom: 0rem;
+	margin-bottom: 0rem;
 }
 
 .page {
-    &__arrow{
-     max-width: 2rem;
-     margin-left: 2rem;
-     transform: translate(0px,-300px);
-    }
+	&__arrow{
+		max-width: 2rem;
+		margin-left: 2rem;
+		transform: translate(0px,-300px);
+	}
 
 	&__input{
-        margin-bottom: 1rem;
-    }
+		margin-bottom: 1rem;
+	}
 }
 </style>
