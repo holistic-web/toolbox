eb0fbb8051e6e0a8df36071a1500966f98897b6b
Andrew Lewis <andrew12lewis@gmail.com>
2020-01-15 00:12:26 +0000

    chore: const

--- toolbox-layout/src/index.js
+++ toolbox-layout/src/index.js
@@ -14,13 +14,12 @@ Object.keys(components).forEach(name => {
 })
 
 // I was not able to install separately which needs to be changed
-// import * as firebase from "firebase/app";
+import * as firebase from "firebase/app";
 
-// import "firebase/analytics";
+import "firebase/analytics";
+// const firebase = require("firebase");
 
-var firebase = require("firebase");
-
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyCYyxjgL9YGvKnzax2EBxEI_bl6KxB2nK8",
     authDomain: "holistic-toolbox.firebaseapp.com",
     databaseURL: "https://holistic-toolbox.firebaseio.com",
@@ -30,9 +29,9 @@ var firebaseConfig = {
     appId: "1:94330098957:web:c87c654651186a555e0c44",
     measurementId: "G-J32Q7R91JH"
   };
+  
 // Initialize Firebase & Analytics
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-// firebase.analytics().logEvent('');
 
 export default components;
