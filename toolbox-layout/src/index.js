364cff12942f32fbba7ce14f69407114b016c0d2
Andrew Lewis <andrew12lewis@gmail.com>
2020-01-15 00:43:40 +0000

    chore: works!

--- toolbox-layout/src/index.js
+++ toolbox-layout/src/index.js
@@ -13,25 +13,19 @@ Object.keys(components).forEach(name => {
 	Vue.component(name, components[name]);
 })
 
-// I was not able to install separately which needs to be changed
 import * as firebase from "firebase/app";
-
 import "firebase/analytics";
-// const firebase = require("firebase");
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCYyxjgL9YGvKnzax2EBxEI_bl6KxB2nK8",
-    authDomain: "holistic-toolbox.firebaseapp.com",
-    databaseURL: "https://holistic-toolbox.firebaseio.com",
-    projectId: "holistic-toolbox",
-    storageBucket: "holistic-toolbox.appspot.com",
-    messagingSenderId: "94330098957",
-    appId: "1:94330098957:web:c87c654651186a555e0c44",
-    measurementId: "G-J32Q7R91JH"
-  };
-  
-// Initialize Firebase & Analytics
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+	apiKey: "AIzaSyCYyxjgL9YGvKnzax2EBxEI_bl6KxB2nK8",
+	authDomain: "holistic-toolbox.firebaseapp.com",
+	databaseURL: "https://holistic-toolbox.firebaseio.com",
+	projectId: "holistic-toolbox",
+	storageBucket: "holistic-toolbox.appspot.com",
+	messagingSenderId: "94330098957",
+	appId: "1:94330098957:web:c87c654651186a555e0c44",
+	measurementId: "G-J32Q7R91JH"
+});
 firebase.analytics();
 
 export default components;
