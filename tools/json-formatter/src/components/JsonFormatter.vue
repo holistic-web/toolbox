cc2d3d551bbcac61c6483398d256e31d52084313
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2020-01-18 18:23:45 +0000

    feat: upgrade json formatter to use new toolbox layout patterns

--- tools/json-formatter/src/components/JsonFormatter.vue
+++ tools/json-formatter/src/components/JsonFormatter.vue
@@ -5,52 +5,6 @@
 			JSON Formatter is a tool to provide quick and easy JSON formatting. Enjoy!
 		</p>
 
-		<div class="JsonFormatter__toolbar">
-
-			<template v-if="!formatted">
-
-				<div class="JsonFormatter__toolbar--left">
-					<p>Add your JSON data here</p>
-				</div>
-
-				<div class="JsonFormatter__toolbar--right">
-					<b-form-group>
-						<b-form-radio-group
-							class="JsonFormatter__toolbar__button"
-							v-model="whitespace"
-							:options="whitespaceOptions"
-							buttons
-							button-variant="outline-primary"
-							size="sm"/>
-					</b-form-group>
-
-					<tool-button
-						class="JsonFormatter__toolbar__button"
-						v-text="'Format'"
-						size="sm"
-						:disabled="!jsonString"
-						@click.native="formatJSON"/>
-				</div>
-			</template>
-
-			<template v-else>
-				<div class="JsonFormatter__toolbar--left"/>
-				<div class="JsonFormatter__toolbar--right">
-					<tool-button
-						size="sm"
-						class="JsonFormatter__toolbar__button"
-						v-text="'Copy Output'"
-						v-clipboard="jsonString"/>
-					<tool-button
-						size="sm"
-						class="JsonFormatter__toolbar__button"
-						v-text="'Reset'"
-						@click.native="reset"/>
-				</div>
-			</template>
-
-		</div>
-
 		<tool-error
 			v-if="errorMessage"
 			class="JsonFormatter__errorMessage"
@@ -60,6 +14,39 @@
 			class="JsonFormatter__input"
 			v-model="jsonString"
 			:options="codemirrorOptions"/>
+
+		<tool-taskbar v-if="jsonString">
+
+			<template v-if="!formatted">
+				<tool-button
+					class="JsonFormatter__button"
+					v-text="'Format'"
+					size="lg"
+					@click.native="formatJSON"/>
+				<b-form-radio-group
+					class="JsonFormatter__button"
+					v-model="whitespace"
+					:options="whitespaceOptions"
+					buttons
+					button-variant="outline-secondary"
+					size="sm"/>
+			</template>
+
+			<template v-else>
+				<tool-button
+					size="lg"
+					class="JsonFormatter__button"
+					v-text="'Copy Output'"
+					v-clipboard="jsonString"/>
+				<tool-button
+					size="sm"
+					class="JsonFormatter__button"
+					variant="outline-secondary"
+					v-text="'Reset'"
+					@click.native="reset"/>
+			</template>
+
+		</tool-taskbar>
 
 	</div>
 </template>
@@ -120,42 +107,29 @@ export default {
 </script>
 
 <style lang="scss">
+@import 'toolbox-layout/src/styles/theme';
 
 .JsonFormatter {
 	display: flex;
 	flex-direction: column;
 	height: 100%;
-
-	&__toolbar {
-		margin-bottom: 1rem;
-		display: flex;
-		flex-direction: row;
-
-		&--left {
-			display: flex;
-			flex-direction: row;
-			flex-grow: 1;
-		}
-
-		&--right {
-			display: flex;
-			flex-direction: row;
-		}
-
-		&__button {
-			justify-self: flex-end;
-			margin-left: 1rem;
-		}
-	}
+	padding: $tool-padding;
 
 	&__errorMessage {
 		margin-bottom: 1rem;
 	}
 
 	&__input {
+		margin-bottom: 150px; // to allow for the taskbar
+
 		.CodeMirror {
 			height: auto;
 		}
+	}
+
+	&__button {
+		margin-left: 1rem;
+		height: fit-content;
 	}
 
 }
