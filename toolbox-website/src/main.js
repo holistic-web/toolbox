5cb18ada98f7abf0f8d27ffc9f3c20456febf86f
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2020-01-19 17:04:14 +0000

    fix: linting on toolbox website

--- toolbox-website/src/main.js
+++ toolbox-website/src/main.js
@@ -2,11 +2,11 @@ import Vue from 'vue';
 import 'toolbox-layout';
 import 'toolbox-layout/dist/toolbox-layout.css';
 import BootstrapVue from 'bootstrap-vue';
-import App from './App.vue';
+import App from './App';
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
-	render: h => h(App),
+	render: h => h(App)
 }).$mount('#app');
