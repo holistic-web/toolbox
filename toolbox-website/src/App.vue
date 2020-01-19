151ba40fedff08078d0915edf3acfb5adefacf9c
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2020-01-19 17:10:13 +0000

    chore: add basic padding

--- toolbox-website/src/App.vue
+++ toolbox-website/src/App.vue
@@ -1,13 +1,21 @@
 <template>
 	<div id="app">
 		<tool-layout name="Home">
-			Test
+			<section class="Website">
+				Test
+			</section>
 		</tool-layout>
 	</div>
 </template>
 
 <style lang="scss">
+@import 'toolbox-layout/src/styles/theme';
+
 #app {
 	font-family: 'Fira Code', monospace;
+}
+
+.Website {
+	padding: $tool-padding;
 }
 </style>
