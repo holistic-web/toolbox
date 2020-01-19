4e0bd3b2a07bff57558f7f18a659a4dd051a35c9
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2020-01-19 16:23:23 +0000

    fix: linting settings

--- toolbox-website/.eslintrc.js
+++ toolbox-website/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-tabs': 0,
-		'indent': 'tabs'
+		'indent': ['error', 'tab']
 	},
 	parserOptions: {
 		parser: 'babel-eslint',
--- tools/json-formatter/.eslintrc.js
+++ tools/json-formatter/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-tabs': 0,
-		'indent': 'tabs'
+		'c
 	},
 	parserOptions: {
 		parser: 'babel-eslint',
