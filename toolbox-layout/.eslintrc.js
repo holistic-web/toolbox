192972587ebfb77d54bcfdf1904d7030e521e5d2
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2020-01-20 01:25:02 +0000

    chore: standardise linting

--- toolbox-layout/.eslintrc.js
+++ toolbox-layout/.eslintrc.js
@@ -5,17 +5,8 @@ module.exports = {
 	},
 	extends: [
 		'plugin:vue/essential',
-		'@vue/airbnb'
+		'toolbox'
 	],
-	rules: {
-		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'no-tabs': 0,
-		'indent': ['error', 'tab'],
-		'comma-dangle': ['error', 'never'],
-		'import/extensions': ['error', 'always', { vue: 'never', js: 'never'}],
-		'global-require': 0
-	},
 	parserOptions: {
 		parser: 'babel-eslint'
 	}
