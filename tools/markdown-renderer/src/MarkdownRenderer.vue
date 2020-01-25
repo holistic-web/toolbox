977961c910a0f91c90189e096512c07af139056b
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2020-01-25 03:51:22 +0000

    feat: finishing touches to markdown renderer

--- tools/markdown-renderer/src/MarkdownRenderer.vue
+++ tools/markdown-renderer/src/MarkdownRenderer.vue
@@ -1,15 +1,22 @@
 <template>
 	<div class="MarkdownRenderer">
 
-		<tool-markdown :markdown="`
-Enter your markdown below:
-		`"/>
+		<section class="MarkdownRenderer__header">
+			<p>Enter your markdown below:</p>
+			<tool-button
+				v-text="'Copy to Clipboard'"
+				v-clipboard="markdown"/>
+		</section>
 
 		<section class="MarkdownRenderer__content">
-			<tool-code v-model="markdown"/>
+			<tool-code
+				class="MarkdownRenderer__editor MarkdownRenderer__content__item MarkdownRenderer__content__item--left"
+				v-model="markdown"
+				:options="codeOptions"/>
 
-			<tool-markdown :markdown="markdown"/>
-
+			<tool-markdown
+				class="MarkdownRenderer__content__item MarkdownRenderer__content__item--right"
+				:markdown="markdown"/>
 		</section>
 
 	</div>
@@ -19,7 +26,11 @@ Enter your markdown below:
 export default {
 	data() {
 		return {
-			markdown: ''
+			markdown: '',
+			codeOptions: {
+				mode: 'Markdown',
+				viewportMargin: Infinity
+			}
 		};
 	}
 };
@@ -33,5 +44,34 @@ export default {
 	flex-direction: column;
 	height: 100%;
 	padding: $tool-padding;
+
+	&__header {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+	&__content {
+		display: flex;
+		flex-direction: row;
+
+		&__item {
+			width: 50%;
+
+			&--left {
+				margin-right: 1rem;
+			}
+
+			&--right {
+				margin-left: 1rem;
+			}
+		}
+	}
+
+	&__editor {
+		.CodeMirror { // to allow the editor to grow with entered text
+			height: auto;
+		}
+	}
 }
 </style>
