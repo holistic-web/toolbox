e44763893b7cc24b2b3b6658bda52bdc3e4c9490
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2020-01-25 03:40:19 +0000

    chore: scaffold markdown renderer

---
+++ tools/markdown-renderer/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="app">
+		<tool-layout name="New Tool">
+			<markdown-renderer/>
+		</tool-layout>
+	</div>
+</template>
+
+<script>
+import MarkdownRenderer from './MarkdownRenderer.vue';
+
+export default {
+	components: {
+		MarkdownRenderer
+	}
+};
+</script>
+
+<style lang="scss">
+#app {
+	font-family: 'Fira Code', monospace;
+}
+</style>
---
+++ tools/markdown-renderer/src/MarkdownRenderer.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="MarkdownRenderer">
+
+		<tool-markdown :markdown="`
+Enter your markdown below:
+		`"/>
+
+		<section class="MarkdownRenderer__content">
+			<tool-code v-model="markdown"/>
+
+			<tool-markdown :markdown="markdown"/>
+
+		</section>
+
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			markdown: ''
+		};
+	}
+};
+</script>
+
+<style lang="scss">
+@import 'toolbox-layout/src/styles/theme';
+
+.MarkdownRenderer {
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	padding: $tool-padding;
+}
+</style>
