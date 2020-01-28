b8268f5a1cc9b10cd8bb55b3b2109092c0ad92b1
Andrew Lewis <andrew12lewis@gmail.com>
2020-01-28 14:27:42 -0800

    feat: some basics

--- tools/number-converter/src/NumberConverter.vue
+++ tools/number-converter/src/NumberConverter.vue
@@ -2,20 +2,35 @@
 	<div class="NumberConverter">
 
 		<tool-markdown :markdown="`
-Documentation for your new tool goes here...
+Enter a number:
 		`"/>
 
+		<tool-code
+			class="NumberConverter__input"
+			v-model="num"
+			:options="codeOptions"
+			:autoSize="true"/>
 
 		<tool-taskbar>
 			<tool-button
 				size="lg"
-				class="JsonFormatter__button"
+				class="NumberConverter__button"
 				v-text="'Copy Output'"
 				v-clipboard="jsonString"/>
 		</tool-taskbar>
 
 	</div>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			num: 0
+		}
+	}
+}
+</script>
 
 
 <style lang="scss">
