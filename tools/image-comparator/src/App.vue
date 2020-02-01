772942b23bce8b9705efefa0d94baccd71f5228d
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2020-02-01 12:03:23 +0000

    chore: add very basic scaffold from template

---
+++ tools/image-comparator/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="app">
+		<tool-layout name="New Tool">
+			<new-tool/>
+		</tool-layout>
+	</div>
+</template>
+
+<script>
+import NewTool from './NewTool.vue';
+
+export default {
+	components: {
+		NewTool
+	}
+};
+</script>
+
+<style lang="scss">
+#app {
+	font-family: 'Fira Code', monospace;
+}
+</style>
