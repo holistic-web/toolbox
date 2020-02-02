3449b6f2ab79424843594de4bbebe7e839fe77d4
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2020-02-02 23:12:18 +0000

    fix: build with new npm org

--- tools/image-comparer/src/ImageComparator.vue
+++ tools/image-comparer/src/ImageComparator.vue
@@ -174,7 +174,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import 'toolbox-layout/src/styles/theme';
+@import '@holistic-web/toolbox-layout/src/styles/theme';
 
 .ImageComparator {
 	display: flex;
