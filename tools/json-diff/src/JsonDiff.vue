9559d94d4f5d9f349f20f7bce7c2f78d951e256c
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2020-02-11 22:36:51 +0000

    chore: scaffold basic json-diff functionality

--- tools/json-diff/src/JsonDiff.vue
+++ tools/json-diff/src/JsonDiff.vue
@@ -3,16 +3,83 @@
 
 		<tool-markdown :markdown="`
 This is tool compares two JSON objects using the library [json-diff](https://www.npmjs.com/package/json-diff).
+Enter two JSON files below:
 		`"/>
 
+		<code v-if="result" v-html="result"/>
 
-		<tool-taskbar>
-			It can be a good idea to add buttons to the taskbar...
+		<section class="JsonDiff__inputs">
+
+			<div class="JsonDiff__inputs__half">
+				<tool-code
+					v-model="json1"
+					:options="codeOptions"
+					:autoSize="true"/>
+			</div>
+
+			<div class="JsonDiff__inputs__half">
+				<tool-code
+					v-model="json1"
+					:options="codeOptions"
+					:autoSize="true"/>
+			</div>
+
+		</section>
+
+
+		<tool-taskbar v-if="showTaskbar">
+
+			<tool-button
+				size="lg"
+				class="JsonDiff__button"
+				v-text="'Diff JSONs'"
+				@click.native="doComparison"/>
+
+			<tool-button
+				class="JsonDiff__button"
+				size="sm"
+				variant="secondary"
+				v-text="'Reset'"
+				@click.native="reset"/>
+
 		</tool-taskbar>
 
 	</div>
 </template>
 
+<script>
+export default {
+	data() {
+		return {
+			json1: '',
+			json2: '',
+			result: null
+		};
+	},
+	computed: {
+		codeOptions() {
+			return {
+				readOnly: !!this.result,
+				lineNumbers: true,
+				mode: 'JSON'
+			};
+		},
+		showTaskbar() {
+			return this.json1 && this.json2;
+		}
+	},
+	methods: {
+		reset() {
+			this.json1 = '';
+			this.json2 = '';
+			this.result = null;
+		},
+		doComparison() {
+			this.result = 'wut';
+		}
+	}
+};
+</script>
 
 <style lang="scss">
 @import '@holistic-web/toolbox-layout/src/styles/theme';
@@ -22,5 +89,20 @@ This is tool compares two JSON objects using the library [json-diff](https://www
 	flex-direction: column;
 	height: 100%;
 	padding: $tool-padding;
+
+	&__inputs {
+		display: flex;
+		flex-direction: row;
+		margin: 0 -1rem;
+
+		&__half {
+			width: 50%;
+			margin: 0 1rem;
+		}
+	}
+
+	&__button {
+		margin-left: 1rem;
+	}
 }
 </style>
