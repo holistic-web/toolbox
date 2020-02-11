21c07b4c94108f5ccf84e86cd66b7bded58e2e21
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2020-02-11 22:14:31 +0000

    chore: run new tool script for json-diff

---
+++ tools/json-diff/src/JsonDiff.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="JsonDiff">
+
+		<tool-markdown :markdown="`
+Documentation for your new tool and any relevant links go here...
+		`"/>
+
+
+		<tool-taskbar>
+			It can be a good idea to add buttons to the taskbar...
+		</tool-taskbar>
+
+	</div>
+</template>
+
+
+<style lang="scss">
+@import '@holistic-web/toolbox-layout/src/styles/theme';
+
+.JsonDiff {
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	padding: $tool-padding;
+}
+</style>
