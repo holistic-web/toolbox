ddd948898a9e73a9d54858714d001d6d493dd32c
Dan Bennett <dghbennett47@gmail.com>
2020-02-11 19:07:18 +0000

    chore: fix bug from template

--- tools/nlp-toolkit/src/NlpToolkit.vue
+++ tools/nlp-toolkit/src/NlpToolkit.vue
@@ -2,20 +2,92 @@
 	<div class="NlpToolkit">
 
 		<tool-markdown :markdown="`
-Documentation for your new tool and any relevant links go here...
-		`"/>
+This is a tool to to perform NLP operations using the library [compromise](https://www.npmjs.com/package/compromise).
+Enter text below: 		`"/>
 
+		<tool-code
+			ref="NlpToolkit__input"
+			class="NlpToolkit__item"
+			v-model="inputText"
+			:autoSize="true"
+			:options="codeOptions"/>
 
-		<tool-taskbar>
-			It can be a good idea to add buttons to the taskbar...
+		<p v-text="resultText"/>
+
+		<tool-taskbar v-if="!!inputText">
+
+			<tool-button
+				class="NlpToolkit__button"
+				size="lg"
+				v-text="'Compute'"
+				@click.native="onComputeClick"/>
+
+			<b-form-group
+				class="NlpToolkit__button"
+				label="NLP Operation"
+				label-for="NlpTookit__operation"
+				label-size="sm">
+				<b-form-select
+					id="NlpTookit__operation"
+					v-model="selectedOperation"
+					:options="nlpOptions"/>
+			</b-form-group>
+
+			<tool-button
+				size="sm"
+				class="NlpToolkit__button"
+				variant="secondary"
+				v-text="'Reset'"
+				@click.native="reset"/>
+
 		</tool-taskbar>
 
 	</div>
 </template>
-
 <script>
-export default {
+import nlp from 'compromise';
 
+const nlpMethods = {
+
+	toPastTense: text => {
+		const doc = nlp(text);
+		doc.verbs().toPastTense();
+		return doc.text();
+	}
+
+};
+
+export default {
+	data() {
+		return {
+			inputText: '',
+			resultText: '',
+			selectedOperation: 'toPastTense',
+			nlpOptions: [
+				{ value: 'toPastTense', text: 'To past tense' }
+			]
+		};
+	},
+	computed: {
+		codeOptions() {
+			return {
+				readOnly: !!this.resultText
+			};
+		}
+	},
+	methods: {
+		reset() {
+			this.inputText = '';
+			this.resultText = '';
+		},
+		onComputeClick() {
+			const nlpFunction = nlpMethods[this.selectedOperation];
+			this.resultText = nlpFunction(this.inputText);
+		}
+	},
+	mounted() {
+		this.$refs.NlpToolkit__input.focus();
+	}
 };
 </script>
 
@@ -28,5 +100,13 @@ export default {
 	flex-direction: column;
 	height: 100%;
 	padding: $tool-padding;
+
+	&__item {
+		margin-bottom: 1rem;
+	}
+
+	&__button {
+		margin-left: 1rem;
+	}
 }
 </style>
