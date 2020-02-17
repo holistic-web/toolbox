2c2b7ed2f3037da2881a487a0c140fb09deaeb9c
Charlie Lancaster <charlie.lancaster@yousoft.com>
2020-02-17 18:38:33 +0000

    Added tool code and error components and data model

--- tools/js-formatter/src/JsFormatter.vue
+++ tools/js-formatter/src/JsFormatter.vue
@@ -5,6 +5,17 @@
 Documentation for your new tool and any relevant links go here...
 		`"/>
 
+		<tool-error
+			v-if="errorMessage"
+			class="JsFormatter__errorMessage"
+			:message="errorMessage"/>
+
+		<tool-code
+			ref="JsFormatter__input"
+			v-model="jsString"
+			:options="codeOptions"
+			:autoSize="true"/>
+
 
 		<tool-taskbar>
 			It can be a good idea to add buttons to the taskbar...
@@ -24,3 +35,28 @@ Documentation for your new tool and any relevant links go here...
 	padding: $tool-padding;
 }
 </style>
+
+<script>
+export default {
+	data() {
+		return {
+			formatted: false,
+			errorMessage: null,
+			minifiedOrig: 'p{font:normal 14px/20px helvetica,arial,sans-serif;color:#333;}.woot{font-weight:bold}',
+			jsString: '',
+			whitespace: '\t',
+			whitespaceOptions: [
+				{ text: 'Tabs', value: '\t' },
+				{ text: 'Spaces', value: 4 },
+				{ text: 'None', value: 0 }
+			]
+		};
+	},
+	computed: {
+
+	},
+	methods: {
+
+	},
+}
+</script>
