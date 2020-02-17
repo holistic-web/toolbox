8bc4aedbd604dcb544adf24e2c4bf236d5e605b0
Charlie Lancaster <charlie.lancaster@yousoft.com>
2020-02-17 18:39:43 +0000

    added js format method

--- tools/js-formatter/src/JsFormatter.vue
+++ tools/js-formatter/src/JsFormatter.vue
@@ -56,7 +56,28 @@ export default {
 
 	},
 	methods: {
+		minified(){
+			//TODO: need to test
+			let vm = this;
+			let tab = vm.minifiedTab;
+			let space = '';
+			let code;
 
+			tab = (/^\d+$/.test(tab) ? parseInt(tab) : 4);
+			code = vm.minifiedOrig
+			.split('{').join(' {\n    ')
+			.split(';').join(';\n    ')
+			.split(',').join(', ')
+			.split('    }').join('}\n')
+			.replace(/\}(.+)/g, '}\n$1')
+			.replace(/\n    ([^:]+):/g, '\n    $1: ')
+			.replace(/([A-z0-9\)])}/g, '$1;\n}');
+			if (tab != 4) {
+			for (;tab != 0;tab--) { space += ' '; }
+			code = code.replace(/\n    /g, '\n'+space);
+			}
+			vm.minifiedNew = code;
+		}
 	},
 }
 </script>
