457dd18b2970d30f5d1e14ce2a6a4d551f65df63
Charlie Lancaster <charlie.lancaster@yousoft.com>
2020-02-17 18:48:07 +0000

    added try catch to format method

--- tools/js-formatter/src/JsFormatter.vue
+++ tools/js-formatter/src/JsFormatter.vue
@@ -98,21 +98,25 @@ export default {
 			let space = '';
 			let code;
 
-			tab = (/^\d+$/.test(tab) ? parseInt(tab) : 4);
-			code = vm.minifiedOrig
-			.split('{').join(' {\n    ')
-			.split(';').join(';\n    ')
-			.split(',').join(', ')
-			.split('    }').join('}\n')
-			.replace(/\}(.+)/g, '}\n$1')
-			.replace(/\n    ([^:]+):/g, '\n    $1: ')
-			.replace(/([A-z0-9\)])}/g, '$1;\n}');
-			if (tab != 4) {
-				for (;tab != 0;tab--) { space += ' '; }
-				code = code.replace(/\n    /g, '\n'+space);
+			try {
+				tab = (/^\d+$/.test(tab) ? parseInt(tab) : 4);
+				code = vm.minifiedOrig
+				.split('{').join(' {\n    ')
+				.split(';').join(';\n    ')
+				.split(',').join(', ')
+				.split('    }').join('}\n')
+				.replace(/\}(.+)/g, '}\n$1')
+				.replace(/\n    ([^:]+):/g, '\n    $1: ')
+				.replace(/([A-z0-9\)])}/g, '$1;\n}');
+				if (tab != 4) {
+					for (;tab != 0;tab--) { space += ' '; }
+					code = code.replace(/\n    /g, '\n'+space);
+				}
+				vm.minifiedNew = code;
+				formatted = true;
+			} catch (err) {
+				this.errorMessage = err.message;
 			}
-			vm.minifiedNew = code;
-			formatted = true;
 		}
 	},
 }
