6bf916a711acfa6045cbc55ef88beb6b20c3991c
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2020-02-21 14:15:35 +0000

    fix: https links

--- toolbox-website/src/Website.vue
+++ toolbox-website/src/Website.vue
@@ -37,43 +37,43 @@ export default {
 				{
 					name: 'Image Comparer',
 					description: 'Finds the differences between images',
-					link: 'http://image-comparer.holistic-toolbox.com',
+					link: 'https://image-comparer.holistic-toolbox.com',
 					image: require('../assets/tools/image-comparer.png')
 				},
 				{
 					name: 'JSON Browser',
 					description: 'Helps analyse JSON data',
-					link: 'http://json-browser.holistic-toolbox.com',
+					link: 'https://json-browser.holistic-toolbox.com',
 					image: require('../assets/tools/json-browser.png')
 				},
 				{
 					name: 'JSON Diff',
 					description: 'Identifies difference between JSON objects',
-					link: 'http://json-diff.holistic-toolbox.com',
+					link: 'https://json-diff.holistic-toolbox.com',
 					image: require('../assets/tools/json-diff.png')
 				},
 				{
 					name: 'JSON Formatter',
 					description: 'A simple, client side JSON formatter.',
-					link: 'http://json-formatter.holistic-toolbox.com',
+					link: 'https://json-formatter.holistic-toolbox.com',
 					image: require('../assets/tools/json-formatter.png')
 				},
 				{
 					name: 'Markdown Renderer',
 					description: 'A browser based markdown renderer.',
-					link: 'http://markdown-renderer.holistic-toolbox.com',
+					link: 'https://markdown-renderer.holistic-toolbox.com',
 					image: require('../assets/tools/markdown-renderer.png')
 				},
 				{
 					name: 'NLP Toolkit',
 					description: 'This is a tool to to perform NLP operations using the library compromise.',
-					link: 'http://nlp-toolkit.holistic-toolbox.com',
+					link: 'https://nlp-toolkit.holistic-toolbox.com',
 					image: require('../assets/tools/number-converter.png')
 				},
 				{
 					name: 'Number Converter',
 					description: 'A simple number converter from decimal to binary, octal and hexadecimal.',
-					link: 'http://number-converter.holistic-toolbox.com',
+					link: 'https://number-converter.holistic-toolbox.com',
 					image: require('../assets/tools/number-converter.png')
 				}
 			]
