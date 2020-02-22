0960b69012313bf7c5d9d685e87dd06e527b7e29
Charlie Lancaster <charlie.lancaster@yousoft.com>
2020-02-22 13:04:33 +0000

    added v-clipboard to tool

--- tools/js-formatter/src/main.js
+++ tools/js-formatter/src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue';
 import '@holistic-web/toolbox-layout';
 import '@holistic-web/toolbox-layout/dist/toolbox-layout.css';
 import BootstrapVue from 'bootstrap-vue';
+import Clipboard from 'v-clipboard';
 import App from './App.vue';
 
 Vue.use(BootstrapVue);
+Vue.use(Clipboard);
 Vue.config.productionTip = false;
 
 new Vue({
