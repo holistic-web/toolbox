81b98516bf38ac1d84eae72f94303be467eaeb29
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2020-02-26 19:59:19 +0000

    chore: update to use new toolbox-layout

--- toolbox-website/src/components/ToolPreview.vue
+++ toolbox-website/src/components/ToolPreview.vue
@@ -25,7 +25,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import '@holistic-web/layout/src/styles/theme';
+@import '@holistic-web/toolbox-layout/src/styles/theme';
 
 .ToolPreview {
 	border-radius: $tool-border-radius;
